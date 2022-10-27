7de330ff944e0b961f04a2abdc94f8d3ac44a87e
unknown <fernandojosuem.15@gmail.com>
2022-10-26 23:18:35 -0500

    trabajo2

--- ejemplos-editor/Ejercicio.java.java
+++
@@ -1,9 +0,0 @@
-
-public class Saludo{
-
-  public static void main(String [] args){
-    
-    System.out.println("Ejercico java con javac y java");
-  }
-
-}
--- ejemplos-editor/HolaMundo.java
+++ ejemplos-editor/HolaMundo.java
@@ -3,7 +3,7 @@ public class HolaMundo{
 
   public static void main(String [] args){
     
-    System.out.println("Ejecutando Java con javac y java");
+    System.out.println("Hola mundo desde edito Gvim");
   }
 
 }
