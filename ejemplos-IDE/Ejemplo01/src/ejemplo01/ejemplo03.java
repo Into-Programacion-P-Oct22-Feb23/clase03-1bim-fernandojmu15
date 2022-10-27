d185e904031a86e6dde3d556b21df5e175210e5a
Fernando Muñoz <fernandojosuem.15@gmail.com>
2022-10-27 18:16:48 -0500

    Ejemplos neatbeans3

---
+++ ejemplos-IDE/Ejemplo01/src/ejemplo01/ejemplo03.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplo01;
+
+/**
+ *
+ * @author DELL
+ */
+public class ejemplo03 {
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Leyendas del mas aya");
+    }
+}
